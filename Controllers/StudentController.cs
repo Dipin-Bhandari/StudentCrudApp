a4d3ced715360de316c0f0a043248a5da4af2e1c
Dipin Bhandari <91279927+Dipin-Bhandari@users.noreply.github.com>
2025-05-27 00:56:47 +0530

    Initial commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentCrudApp.Models;
+using StudentCrudApp.Data;
+using System.Linq;
+
+namespace StudentCrudApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(StudentRepository.Students);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                student.Id = StudentRepository.Students.Count > 0 ? StudentRepository.Students.Max(s => s.Id) + 1 : 1;
+                StudentRepository.Students.Add(student);
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var student = StudentRepository.Students.FirstOrDefault(s => s.Id == id);
+            return student == null ? NotFound() : View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student updatedStudent)
+        {
+            var student = StudentRepository.Students.FirstOrDefault(s => s.Id == updatedStudent.Id);
+            if (student != null)
+            {
+                student.Name = updatedStudent.Name;
+                student.School = updatedStudent.School;
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var student = StudentRepository.Students.FirstOrDefault(s => s.Id == id);
+            return student == null ? NotFound() : View(student);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var student = StudentRepository.Students.FirstOrDefault(s => s.Id == id);
+            if (student != null)
+            {
+                StudentRepository.Students.Remove(student);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/StudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using StudentCrudApp.Models;
+using System.Collections.Generic;
+
+namespace StudentCrudApp.Data
+{
+    public static class StudentRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>();
+    }
+}
